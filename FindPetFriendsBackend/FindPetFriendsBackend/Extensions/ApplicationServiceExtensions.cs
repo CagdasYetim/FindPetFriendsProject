d3adc4c9bfebe2a2fcc00d23aa187f603d20fd23
Cagdas Yetim <cagdas.yetim@gmail.com>
2021-04-11 16:36:05 +0200

    Backend Part 1 (UserRepository.cs)

--- FindPetFriendsBackend/FindPetFriendsBackend/Extensions/ApplicationServiceExtensions.cs
+++ FindPetFriendsBackend/FindPetFriendsBackend/Extensions/ApplicationServiceExtensions.cs
@@ -14,6 +14,7 @@ namespace API.Extensions
         public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
         {
             services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
             services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
             services.AddDbContext<DataContext>(options =>
             {
---
+++ FindPetFriendsBackend/FindPetFriendsBackend/Interfaces/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FindPetFriendsBackend.Interfaces
+{
+    public interface IUserRepository
+    {
+        void Update(AppUser user);
+
+        Task<AppUser> GetUserByIdAsync(int id);
+        Task<AppUser> GetUserByUsernameAsync(string username);
+    }
+}
