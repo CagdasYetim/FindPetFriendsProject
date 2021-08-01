627f9e0b22847da34c9fa97e5f0a863de14f6b93
Cagdas Yetim <cagdas.yetim@gmail.com>
2021-08-01 22:02:11 +0200

    PWA improvement

--- FindPetFriendsClientWithoutPWA/src/app/models/checkboxTask.ts
+++
@@ -1,9 +0,0 @@
-import { ThemePalette } from "@angular/material/core";
-
-export interface CheckboxTask {
-  name: string;
-  label:string;
-  completed: boolean;
-  color: ThemePalette;
-  subtasks?: CheckboxTask[];
-}
--- FindPetFriendsClientWithoutPWA/src/app/models/dialogModel.ts
+++
@@ -1,8 +0,0 @@
-export interface DialogModel {
-  label: string,
-  text: string,
-  name: string,
-  buttonCloseLabel: string,
-  buttonOkLabel: string,
-  inputs: { type: string,mapEnable:boolean, label: string, data: any }[]
-}
